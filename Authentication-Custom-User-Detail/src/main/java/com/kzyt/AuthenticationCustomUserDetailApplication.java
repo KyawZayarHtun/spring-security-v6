0aac6b95121cc78cf54f1014cd2953a9168a06a0
KyawZayarHtun <kyawzayarhtun@agbcommunicating.com>
2024-01-14 11:02:28 +0630

    base project for custom user detail service

---
+++ Authentication-Custom-User-Detail/src/main/java/com/kzyt/AuthenticationCustomUserDetailApplication.java
@@ -0,0 +1,13 @@
+package com.kzyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationCustomUserDetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationCustomUserDetailApplication.class, args);
+	}
+
+}
